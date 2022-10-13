df02f3c35e6fe67b98a7289fdc2333d7068f513c
Kubanych22 <30312496+Kubanych22@users.noreply.github.com>
2022-10-13 10:10:56 +0600

    Test

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function myFirstApp(name, age) {
+    alert(`Привет! Меня зовут ${name}. Это моя первая программа.`);
+    showSkills();
+    checkAge();
+    calcPow(5);
+    function showSkills() {
+        let skills = ['html', 'css', 'js'];
+        console.log('Я владею навыками:')
+        for (let i = 0; i < skills.length; i++) {
+            console.log(skills[i]);
+        }
+    }
 
+    function checkAge() {
+        if (age > 18) {
+            console.log('У тебя отличные шансы стать фронтенд разработчиком!')
+        } else {
+            console.log('Круто, что ты так рано задумался о своем будущем!')
+        }
+    }
+
+    function calcPow(num) {
+        console.log(Math.pow(num, 2));
+    }
+}
+myFirstApp('Ivan', 14);
